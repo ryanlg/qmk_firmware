169628d6c06362e9dabb05e20127aa6abb3ad440
Ryan Liang <rl@ryanl.io>
2020-10-02 14:51:15 -0500

    @ryanl: ACTUALLadd ryan kemap to savage65

--- keyboards/cannonkeys/savage65/keymaps/ryan/keymap.c
+++ keyboards/cannonkeys/savage65/keymaps/ryan/keymap.c
@@ -53,23 +53,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |Ctrl|Alt |Cmd |        Space          |Ctrl|Alt|FN |Lef|Dow|Rig |
    * `----------------------------------------------------------------'
    */
-[DEFAULT_LAYER] = LAYOUT_ansi(
+[DEFAULT_LAYER] = LAYOUT_default(
 
-    FN     ,KC_1   ,KC_2   ,KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSPC,KC_GRV ,\
+    FN     ,KC_1   ,KC_2   ,KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSPC,KC_DEL,KC_GRV ,\
     KC_TAB ,KC_Q   ,KC_W   ,KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSLS,KC_DEL ,\
-    KC_ESC ,KC_A   ,KC_S   ,KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,         KC_ENT,KC_PGUP,\
-    KC_LSFT,        KC_Z   ,KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,  KC_UP,KC_PGDN,\
-    LRCTRL ,KC_LALT,KC_LGUI,                KC_SPC ,                        KC_RCTL,KC_RALT,   FN  ,KC_LEFT,KC_DOWN,KC_RGHT
+    KC_ESC ,KC_A   ,KC_S   ,KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_NUBS, KC_ENT,KC_PGUP,\
+    KC_LSFT,KC_NUHS,KC_Z   ,KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,  KC_UP,KC_PGDN,\
+    LRCTRL ,KC_LALT,KC_LGUI,                KC_SPC ,                        KC_RCTL,           FN  ,KC_LEFT,KC_DOWN,KC_RGHT
 
     ),
 
-[FUCK_WINDOWS_LAYER] = LAYOUT_ansi(
+[FUCK_WINDOWS_LAYER] = LAYOUT_default(
 
-    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,\
+    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,\
     _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_MPRV,\
-    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,        _______,KC_MNXT,\
-    _______,        _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,\
-    KC_LCTL,KC_LGUI,KC_LALT,                _______,                        _______,_______,_______,_______,_______,_______
+    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_MNXT,\
+    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,\
+    KC_LCTL,KC_LGUI,KC_LALT,                _______,                        _______,        _______,_______,_______,_______
 
     ),
 
@@ -87,13 +87,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `----------------------------------------------------------------'
    */
 
-[FUNCTION_LAYER] = LAYOUT_ansi(
+[FUNCTION_LAYER] = LAYOUT_default(
 
-    _______,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,KC_DEL ,KC_MPLY,\
+    _______,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,KC_DEL ,_______,KC_MPLY,\
     _______,_______,_______,_______,_______,_______,_______,KC_BRID,KC_BRIU,_______,_______,_______,_______,_______,KC_MRWD,\
-    _______,_______,_______,_______,_______,_______,KC_LEFT,KC_DOWN,  KC_UP,KC_RGHT,_______,_______,        KC_CAPS,KC_MFFD,\
-    _______,_______,_______,_______,_______,_______,_______,KC_MUTE,KC_VOLD,KC_VOLU,_______,        _______,_______,MISNCTL,\
-    _______,_______,_______,                KC_MUTE,                        TG_L   ,_______,_______,_______,_______,LNCHPAD
+    _______,_______,_______,_______,_______,_______,KC_LEFT,KC_DOWN,  KC_UP,KC_RGHT,_______,_______,_______,KC_CAPS,KC_MFFD,\
+    _______,_______,_______,_______,_______,_______,_______,_______,KC_MUTE,KC_VOLD,KC_VOLU,_______,_______,_______,MISNCTL,\
+    _______,_______,_______,                KC_MUTE,                        TG_L   ,        _______,_______,_______,LNCHPAD
 
     ),
 
